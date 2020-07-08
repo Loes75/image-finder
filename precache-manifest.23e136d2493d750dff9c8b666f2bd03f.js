a4acb0708c96b2f9f7c84573a3a7f0ac468981eb
LauraOchoa <lauraochoae7@gmail.com>
2020-07-07 22:41:11 -0500

    Updates

---
+++ precache-manifest.23e136d2493d750dff9c8b666f2bd03f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60c51457566ae0b9ac36ad9378b9891",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "028e1071d89499fb05a9",
+    "url": "/image-finder/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd83d4a8bfa9e489c302",
+    "url": "/image-finder/static/js/2.7ede2706.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-finder/static/js/2.7ede2706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028e1071d89499fb05a9",
+    "url": "/image-finder/static/js/main.0606c2f6.chunk.js"
+  },
+  {
+    "revision": "1efe7332d5f9e3f659c7",
+    "url": "/image-finder/static/js/runtime-main.ef03d61f.js"
+  }
+]);
